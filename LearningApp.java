f7e188248246726af22092bde85ed4979c0308ce
KIRITA Ren <r.kirita.shukatsu@gmail.com>
2026-06-03 18:06:56 +0900

    初回コミット：Java学習アプリのメニューを作成

---
+++ LearningApp.java
@@ -0,0 +1,12 @@
+public class LearningApp{
+  public static void main(String[] args){
+    System.out.println("===Java学習アプリ===");
+    System.out.println("1. Javaとは");
+    System.out.println("2. 変数");
+    System.out.println("3. 演算子");
+    System.out.println("4. 条件分岐");
+    System.out.println("5. 繰り返し");
+    System.out.println("0. 終了");
+    
+  }
+}
