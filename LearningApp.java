eb1d048138ba2aec75ff2bf5bcaf0ba508df282b
KIRITA Ren <r.kirita.shukatsu@gmail.com>
2026-06-03 18:29:14 +0900

    ユーザー入力機能を追加

--- LearningApp.java
+++ LearningApp.java
@@ -1,5 +1,9 @@
+import java.util.Scanner; //Scannerって書いたら、java/util/Scannerのことだよって意味
+
 public class LearningApp{
   public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in); //System.inはキーボードからの入力を表す
+
     System.out.println("===Java学習アプリ===");
     System.out.println("1. Javaとは");
     System.out.println("2. 変数");
@@ -7,6 +11,12 @@ public class LearningApp{
     System.out.println("4. 条件分岐");
     System.out.println("5. 繰り返し");
     System.out.println("0. 終了");
+
+    int choice = scanner.nextInt();
+
+    System.out.println("あなたが選んだ番号は " + choice + " です。 ");
+
+    scanner.close();
     
   }
 }
