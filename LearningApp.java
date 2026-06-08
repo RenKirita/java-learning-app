45a88459c481175bea63096a81492bd0b3a920a8
KIRITA Ren <r.kirita.shukatsu@gmail.com>
2026-06-09 06:41:14 +0900

    Lesson配列を使って学習項目を管理

--- LearningApp.java
+++ LearningApp.java
@@ -14,42 +14,29 @@ public class LearningApp {
     System.out.println("5. 繰り返し");
     System.out.println("0. 終了");
 
+    Lesson[] lessons = new Lesson[5];
+
+    lessons[0] = new Lesson("1.Javaとは", "Javaはプログラミング言語です。");
+    lessons[1] = new Lesson("2. 変数", "変数はデータを保存する箱です。");
+    lessons[2] = new Lesson("3. 演算子", "演算子は計算や比較を行うための記号です。");
+    lessons[3] = new Lesson("4. 条件分岐", "条件分岐は条件によって処理を変える仕組みです。");
+    lessons[4] = new Lesson("5. 繰り返し", "繰り返しは同じ処理を何度も実行する仕組みです。");
+
     while (true) {
 
-      Lesson lesson = null;
-
-      choice = scanner.nextInt(); //初期化されてない変数（入力されてない）やつがあると、エラーが発生するから、これを先に書かないとエラーが発生する
+      choice = scanner.nextInt(); // 初期化されてない変数（入力されてない）やつがあると、エラーが発生するから、これを先に書かないとエラーが発生する
       System.out.println("あなたが選んだ番号は " + choice + " です。 ");
 
-      if(choice == 0){
+      if (choice == 0) {
         break;
       }
-      if (choice == 1) {
-        lesson = new Lesson("1. Javaとは", "Javaはプログラミング言語です。");
-
-      } else if (choice == 2) {
-        lesson = new Lesson("2. 変数", "変数はデータを保存する箱です。");
-
-      } else if (choice == 3) {
-        lesson = new Lesson("3. 演算子", "演算子は計算や比較を行うための記号です。");
-
-      } else if (choice == 4) {
-        lesson = new Lesson("4. 条件分岐", "条件分岐は条件によって処理を変える仕組みです。");
-        
-      } else if (choice == 5) {
-        lesson = new Lesson("5. 繰り返し", "繰り返しは同じ処理を何度も実行する仕組みです。");
-        
+      if (1 <= choice && choice <= 5) {
+        lessons[choice - 1].showInfo();
       } else {
         System.out.println("存在しない番号です。");
       }
-
-      if(lesson != null)
-      {
-        lesson.showInfo();
-      }
     }
     System.out.println("終了します。");
-    
 
     scanner.close();
 
