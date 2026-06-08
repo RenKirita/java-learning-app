96313c58a67d3e766fb83c2fa9a9a08b231425fb
KIRITA Ren <r.kirita.shukatsu@gmail.com>
2026-06-09 05:05:32 +0900

    Lessonクラスを利用して学習項目を表示

--- LearningApp.java
+++ LearningApp.java
@@ -16,6 +16,8 @@ public class LearningApp {
 
     while (true) {
 
+      Lesson lesson = null;
+
       choice = scanner.nextInt(); //初期化されてない変数（入力されてない）やつがあると、エラーが発生するから、これを先に書かないとエラーが発生する
       System.out.println("あなたが選んだ番号は " + choice + " です。 ");
 
@@ -23,51 +25,34 @@ public class LearningApp {
         break;
       }
       if (choice == 1) {
-        Lesson javaLesson = new Lesson("1. Javaとは", "Javaはプログラミング言語です。");
-        javaLesson.showInfo();
+        lesson = new Lesson("1. Javaとは", "Javaはプログラミング言語です。");
 
       } else if (choice == 2) {
-        Lesson variableLesson = new Lesson("2. 変数", "変数はデータを保存する箱です。");
-        variableLesson.showInfo();
+        lesson = new Lesson("2. 変数", "変数はデータを保存する箱です。");
 
       } else if (choice == 3) {
-        Lesson OperatorLesson = new Lesson("3. 演算子", "演算子は計算や比較を行うための記号です。");
-        OperatorLesson.showInfo();
+        lesson = new Lesson("3. 演算子", "演算子は計算や比較を行うための記号です。");
 
       } else if (choice == 4) {
-        Lesson TermsLesson = new Lesson("4. 条件分岐", "条件分岐は条件によって処理を変える仕組みです。");
-        TermsLesson.showInfo();
+        lesson = new Lesson("4. 条件分岐", "条件分岐は条件によって処理を変える仕組みです。");
         
       } else if (choice == 5) {
-        Lesson RepetitionLesson = new Lesson("5. 繰り返し", "繰り返しは同じ処理を何度も実行する仕組みです。");
-        RepetitionLesson.showInfo();
+        lesson = new Lesson("5. 繰り返し", "繰り返しは同じ処理を何度も実行する仕組みです。");
         
       } else {
-        showInvalidNumberInfo();
+        System.out.println("存在しない番号です。");
+      }
+
+      if(lesson != null)
+      {
+        lesson.showInfo();
       }
     }
     System.out.println("終了します。");
+    
 
     scanner.close();
 
-  }
-  public static void showJavaInfo(){
-    System.out.println("Javaはプログラミング言語です"); //staticとはインスタンスを作らなくても使えるもの
-  }
-  public static void showVariableInfo(){
-    System.out.println("変数はデータを保存する箱です。"); //メソッド名は区切りを大文字にする（キャメルケース）
-  }
-  public static void showOperatorInfo(){
-    System.out.println("演算子は計算や比較を行うための記号です。");
-  }
-  public static void showTermsInfo(){
-    System.out.println("条件分岐は条件によって処理を変える仕組みです。");
-  }
-  public static void showRepetitionInfo(){
-    System.out.println("繰り返しは同じ処理を何度も実行する仕組みです。");
-  }
-  public static void showInvalidNumberInfo(){
-    System.out.println("存在しない番号です。");
   }
 
 }
