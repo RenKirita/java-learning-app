80b51421ebe915eea760fd4a3c4e249b19639e06
KIRITA Ren <r.kirita.shukatsu@gmail.com>
2026-06-16 15:03:20 +0900

    クイズ機能と正誤判定を追加

--- LearningApp.java
+++ LearningApp.java
@@ -16,26 +16,41 @@ public class LearningApp {
 
     Lesson[] lessons = new Lesson[5];
 
-    lessons[0] = new Lesson("1.Javaとは", "Javaはプログラミング言語です。");
-    lessons[1] = new Lesson("2. 変数", "変数はデータを保存する箱です。");
-    lessons[2] = new Lesson("3. 演算子", "演算子は計算や比較を行うための記号です。");
-    lessons[3] = new Lesson("4. 条件分岐", "条件分岐は条件によって処理を変える仕組みです。");
-    lessons[4] = new Lesson("5. 繰り返し", "繰り返しは同じ処理を何度も実行する仕組みです。");
+    lessons[0] = new Lesson("1.Javaとは", "Javaはプログラミング言語です。", "Javaはプログラミング言語ですか？", "yes");
+    lessons[1] = new Lesson("2. 変数", "変数はデータを保存する箱です。", "変数はデータを保存する箱ですか？", "yes");
+    lessons[2] = new Lesson("3. 演算子", "演算子は計算や比較を行うための記号です。", "演算子は計算や比較を行うための記号ですか？", "yes");
+    lessons[3] = new Lesson("4. 条件分岐", "条件分岐は条件によって処理を変える仕組みです。", "条件分岐は条件によって処理を変える仕組みですか？", "yes");
+    lessons[4] = new Lesson("5. 繰り返し", "繰り返しは同じ処理を何度も実行する仕組みです。", "繰り返しは同じ処理を何度も実行する仕組みですか？", "yes");
 
     while (true) {
 
-      choice = scanner.nextInt(); // 初期化されてない変数（入力されてない）やつがあると、エラーが発生するから、これを先に書かないとエラーが発生する
+      choice = scanner.nextInt(); // 初期化されてない変数（入力されてないやつ）があると、エラーが発生するから、これを先に書かないとエラーが発生する
       System.out.println("あなたが選んだ番号は " + choice + " です。 ");
+      scanner.nextLine();
 
       if (choice == 0) {
         break;
       }
       if (1 <= choice && choice <= 5) {
         lessons[choice - 1].showInfo();
+        System.out.println("Enterを押すと問題へ進みます。");
+
+        scanner.nextLine();
+
+        lessons[choice - 1].showQuiz();
+
+        String userAnswer = scanner.nextLine();
+        
+        if (lessons[choice - 1].checkAnswer(userAnswer) == true) {
+          System.out.println("正解です。");
+        } else {
+          System.out.println("不正解です。");
+        }
       } else {
         System.out.println("存在しない番号です。");
       }
     }
+
     System.out.println("終了します。");
 
     scanner.close();
--- Lesson.java
+++ Lesson.java
@@ -1,14 +1,29 @@
 public class Lesson{
   String title;
   String description;
+  String quiz;
+  String answer;
 
-  Lesson(String title, String description){
+  Lesson(String title, String description, String quiz, String answer){
     this.title = title; //引数titleはコンストラクタ実行中だけ存在する引数
     this.description = description; //だから、メンバ変数に代入する必要がある
+    this.quiz = quiz;
+    this.answer = answer;
   }
 
   public void showInfo(){
     System.out.println(this.title);
     System.out.println(this.description);
-  };
+  }
+
+  public void showQuiz(){
+    System.out.println(this.quiz);
+    // System.out.println(this.answer); 答えは一緒に表示しない
+  }
+
+  public boolean checkAnswer(String userAnswer) {
+
+    return userAnswer.equals(this.answer);
+
+} //役割を完全に分けて、「正誤の判定」はLessonクラス、「表示」はLearningApppクラスとした
 }
